8a5aeae1457863f57fe92d15b27a1df928c0ace9
adriana bonilla <apbonillab@gmail.com>
2019-02-09 20:35:57 -0500

    se modifica nombre bd

--- app/services/concurso.srv.js
+++ app/services/concurso.srv.js
@@ -4,20 +4,23 @@ const mysql = require('mysql');
 const connection = require('../../db');
 const security = require('./security.srv');
 
-module.exports.crear = (nombre,fecha_inicio,fecha_fin,valor,guion,recomendaciones,url,banner,idusuario,success,error)=>{
+module.exports.crear = (nombre,fecha_inicio,fecha_fin,valor,guion,recomendaciones,url,banner,idcuentaadmin,success,error)=>{
     let userData = [[nombre,fecha_inicio,fecha_fin,valor,guion,recomendaciones,url,banner]];
     connection.query(`insert into concursos (nombre,fecha_inicio,fecha_fin,valor,guion,recomendaciones,url,banner) values ? `,
     [userData],function(err,result,fields){
         if(err){
             error(err);
         }
-        let concurso = [[result.insertId,idusuario]];
+        let concurso = [[result.insertId,idcuentaadmin]];
         connection.query(`insert into gestion_concurso (concursos,creador) values ? `,
         [concurso],function(err,result,fields){
             if(err){
                 error(err);
+            }else{
+                success(result);
             }
-            success(result);
+         
+            
         })
     })
 }
@@ -27,8 +30,10 @@ module.exports.mostrarTodos = (success,error)=>{
     connection.query(`select * from concursos`,function(err,result,fields){
         if(err){
             error(err);
+        }else{
+            success(result);
         }
-        success(result);
+    
     })
 }
 
@@ -36,12 +41,14 @@ module.exports.mostrarConcursoXid = (idconcursos,success,error)=>{
     connection.query(`select * from concursos where idconcursos = ${idconcursos}`,function(err,result,fields){
         if(err){
             error(err);
+        }else{
+            success(result);
         }
-        success(result);
+
     })
 }
 
-module.exports.mostrarConcursosXUsuario = (idusuario,success,error)=>{
+module.exports.mostrarConcursosXUsuario = (idcuentaadmin,success,error)=>{
     connection.query(`Select c.idconcursos 'idconcurso',
     c.fecha_inicio 'fecha_inicio', c.fecha_fin 'fecha_fin',
     c.valor 'valor',
@@ -52,11 +59,13 @@ module.exports.mostrarConcursosXUsuario = (idusuario,success,error)=>{
     c.nombre 'nombre'
     from gestion_concurso as gc
     inner join concursos as c on c.idconcursos = gc.concursos
-    where gc.creador = ${idusuario}`,function(err,result,fields){
+    where gc.creador = ${idcuentaadmin}`,function(err,result,fields){
         if(err){
             error(err);
+        }else{
+            success(result);
         }
-        success(result);
+
     })
 }
 
@@ -74,10 +83,31 @@ module.exports.eliminarArchivosXconcurso = (idconcursos,success,error)=>{
             connection.query(`delete from concursos where idconcursos = ${idconcursos}`,function(err,result,fields){
                 if(err){
                     error(err);
+                }else{
+                    success(result);
                 }
-                success(result);
             })
         })
     })
 }
 
+module.exports.editar = (idconcursos,nombre,fecha_inicio,fecha_fin,valor,guion,recomendaciones,url,banner,success,error)=>{
+    connection.query(`update concursos set
+    nombre="${nombre}",
+    fecha_inicio = "${fecha_inicio}",
+    fecha_fin="${fecha_fin}",
+    valor="${valor}",
+    guion="${guion}",
+    correo="${correo}",
+    recomendaciones=${recomendaciones},
+    url=${url},
+    banner=${banner},
+    where idconcursos = ${idconcursos}`,function(err,result,fields){
+         if(err){
+             error(err);
+         }else{
+            success(result);
+         }
+        
+     });
+ }

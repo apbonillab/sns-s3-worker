f945f499314b08c3e72b8b04021267c1d4d9f36d
Alexis  Montana Ramirez <amontanar@axacolpatria.co>
2019-02-19 13:33:39 -0500

    subir archivos back

--- app/controller/archivos.ctrl.js
+++ app/controller/archivos.ctrl.js
@@ -1,28 +1,64 @@
 'use strict'
 var express = require('express')
-var routr = express.Router();
+const fileUpload = require('express-fileupload');
+//var fs  = require('fs');
+// default options
+var app = express();
+app.use(fileUpload());
+
+//var routr = express.Router();
 var archivosServices = require('../services/archivos.srv.js');
 var convertirServices = require('../services/conversor.srv.js');
 
-routr.post('/creacion',(req, res) => {
-    archivosServices.crearArchivo(
-        req.body.observaciones,
-        req.body.idlocutor,
-        req.body.voz_inicial,
-        req.body.concurso,
-        req.body.extension,
-        function (concurso) {
-            res.status(201).send({'message':'Archivo creado exitosamente'})
-        },function(error){
-            console.log(error);
-            res.status(500).send(error);
+app.post('/creacion',(req, res) => {
+
+    // var idCurso  = 13;
+    // // vididacion para generar un nuevo directerio con el nombre del archivo 
+    //  var  destination= function (req, file, callback) {
+    //     var dir = './uploads';
+    //     if (!fs.existsSync(dir)) {
+    //       fs.mkdirSync(dir);
+    //     }
+    //     callback(null, dir);
+    // }
+
+    if (!req.files) {
+        return res.status(400).send('no se adjunto ningun audio');
+      }
+      var archivo =  req.files.audio;
+
+      // para validar la extencion 
+    //   var nombreCompleto =  archivo.name.split('.'); 
+    //   var  extencion = nombreCompleto[ nombreCompleto.length - 1];
+
+      // modificar nombre de archivo 
+
+      archivo.mv('./uploads/' + archivo.name, function(err) {
+        if (err)
+          return res.status(500).send(err);
+        res.send({respuesta:'se subio el audio ', body :req.body });
+      });
+
+       console.log(req.body)
+    //   return res.status(200).send(extencion);
+    // archivosServices.crearArchivo(
+    //     req.body.observaciones,
+    //     req.body.idlocutor,
+    //     req.body.voz_inicial,
+    //     req.body.concurso,
+    //     req.body.extension,
+    //     function (concurso) {
+    //         res.status(201).send({'message':'Archivo creado exitosamente'})
+    //     },function(error){
+    //         console.log(error);
+    //         res.status(500).send(error);
             
-        }
-    )
+    //     }
+    // )
 
 })
 
-routr.get('/obtener/concurso/:concurso', (req, res) => {
+app.get('/obtener/concurso/:concurso', (req, res) => {
     archivosServices.obtenerArchxConcurso(
         req.params.concurso,
         function (archivos) {
@@ -35,7 +71,7 @@ routr.get('/obtener/concurso/:concurso', (req, res) => {
 
 })
 
-routr.get('/obtener/concurso/url/:url', (req, res) => {
+app.get('/obtener/concurso/url/:url', (req, res) => {
     archivosServices.obtenerArchxConcursoURL(
         req.params.url,
         function (archivos) {
@@ -67,4 +103,4 @@ cron.schedule('* * * * *', () => {
 
 
 
-module.exports = routr;
+module.exports = app;

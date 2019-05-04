d0e376f63f5c3446d0ef6d5fb135be3dcec3fcf8
adriana bonilla <apbonillab@gmail.com>
2019-05-03 22:24:05 -0500

    sendgrid

---
+++ Worker/archivos.srv.js
@@ -0,0 +1,256 @@
+'use strict';
+var conf = require('./config.js');
+const RUTA_GESTOR_ARCHIVOS = conf.get('ruta_gestion_archivos');
+const uuidv4 = require('uuid/v4');
+var AWS = require('aws-sdk');
+var guardarEnS3= require('./s3Storage');
+
+
+AWS.config.update({
+  region: 'us-east-1',
+  accessKeyId:process.env.ACCES_KEY_ID,
+  secretAccessKey:process.env.SECRET_ACCESS_KEY
+});
+var ddb = new AWS.DynamoDB.DocumentClient({apiVersion: '2018-03-24'});
+var email = require('sendgrid')(process.env.SENDGRID_API_KEY);
+
+
+const crearArchivo = (observaciones, nombre,segundonombre,apellido,segundoapellido, concurso,url,file,correo, success, error) => {
+  var archivo = file;
+  var nombreCompleto = archivo.name.split('.');
+  var extension = nombreCompleto[nombreCompleto.length - 1];
+  let idarchivo=uuidv4();
+  let urlInicial = url.split('concurso/url/')[1];
+  //archivo.mv(RUTA_GESTOR_ARCHIVOS + concurso + '/inicial/' + archivo.name + "_" + concurso +"_"+idarchivo + "." + extension, function (err) {
+  archivo.mv(RUTA_GESTOR_ARCHIVOS + concurso + '/inicial/' + idarchivo + '.' + extension, function (err) {
+    if (err)
+      error(err);
+    else{
+      let d = new Date();
+      let dateAudit = d.getTime();
+      let voz_convertida = null;
+      let estado = 'Sin convertir';
+      //let ruta="concurso-" + concurso;
+      let filename=`concurso-${concurso}/inicial/${idarchivo}.${extension}`;
+      console.log('extension --> '+extension);
+      guardarEnS3.saveFileToS3(filename,archivo.data,true);
+      if (extension.toLowerCase() === 'mp3') {
+        console.log('es igual');
+        filename=`concurso-${concurso}/convertida/${idarchivo}.${extension}`;
+        guardarEnS3.saveFileToS3(filename,archivo.data, false);
+        archivo.mv(RUTA_GESTOR_ARCHIVOS + concurso + '/convertida/'+ idarchivo + '.' + extension, function (err) {
+          if (err) {
+            console.log(err);
+            error(err);
+          }else{
+            voz_convertida = idarchivo+'.mp3';
+            estado = 'Convertida';
+            save(observaciones, nombre,segundonombre,apellido,segundoapellido, estado, idarchivo + '.' + extension, concurso, dateAudit, extension, voz_convertida,correo,urlInicial,error,success);
+            envioCorreo(correo, url);
+          }
+        });
+                
+      }else{
+        save(observaciones, nombre,segundonombre,apellido,segundoapellido, estado, idarchivo + '.' + extension, concurso, dateAudit, extension, voz_convertida,correo,urlInicial,error,success);
+      }
+            
+           
+    }
+  });
+    
+};
+
+function save( observaciones, nombre,segundonombre,apellido,segundoapellido, estado, voz_inicial, concurso, dateAudit, extension, voz_convertida,correo,url,error,success ){
+  let idarchivo = uuidv4();
+  var params = {
+    TableName: 'archivos',
+    Item: {
+      idarchivos: idarchivo,
+      observaciones: observaciones?observaciones:'Sin observaciones',
+      nombre: nombre,
+      apellido: apellido,
+      segundonombre:segundonombre?segundonombre:' ',
+      segundoapellido:segundoapellido?segundoapellido:' ',
+      estado:estado,
+      voz_inicial: voz_inicial,
+      idconcurso: concurso,
+      fecha: dateAudit,
+      ext_voz_inicial:extension,
+      voz_convertida: voz_convertida?voz_convertida:'Sin convertir',
+      correo:correo,
+      ruta: url
+    }
+  };
+  console.log('parametros save: ',params);
+
+  ddb.put(params,function(err,result){
+    if(err){
+      console.log('Error save: ',err);
+      error(err);
+    }else{
+      success(result);
+    }
+  });
+}
+
+const obtenerArchxConcursoURL = (urlConcurso,start,limit, success, error) => {
+  var params = {
+    TableName: 'archivos',
+    FilterExpression : 'ruta = :url',
+    ExpressionAttributeValues : {':url': urlConcurso} 
+  };
+  ddb.scan(params,function(err,result){
+    if(err){
+      error(err);
+    }else{
+      console.log('-- archivosXurl '+JSON.stringify(result));
+      success(result);
+    }
+    
+  });
+
+};
+
+
+const obtenerArchxConcurso = (idconcurso,start,limit, success, error) => {
+
+  var params = {
+    TableName: 'archivos',
+    FilterExpression : 'idconcurso = :idconcurso',
+    ExpressionAttributeValues : {':idconcurso': idconcurso} 
+  };
+  ddb.scan(params,function(err,result){
+    if(err){
+      error(err);
+    }else{
+      console.log('-- archivosXcONCURSO'+JSON.stringify(result));
+      result.url = result.ruta;
+      console.log('-- archivosXcONCURSO_____'+JSON.stringify(result));
+      success(result);
+    }
+    
+  });
+};
+
+const actualizarEstado = (idarchivos, voz_convertida, correo, ruta,idconcurso, success, error) => {
+  var nombreCompleto = voz_convertida.split('.');
+  var params = {
+    TableName: 'archivos',
+    Key:{
+      idarchivos: idarchivos,
+      idconcurso: idconcurso
+    },
+    UpdateExpression : 'set estado = :estado, voz_convertida = :convertida',
+    ExpressionAttributeValues : {
+      ':estado': 'Convertida',
+      ':convertida':nombreCompleto[0]+'.mp3'
+    },
+    'ReturnValues' : 'ALL_NEW'
+  };
+
+  ddb.update(params,function(err,result){
+    if(err){
+      console.log(err);
+      error(err);
+    }else{
+      console.log('--  ACTUALIZAR ESTADO:'+JSON.stringify(result));
+      envioCorreo(correo, result.Attributes.ruta);
+      success('ok');
+    }
+        
+  });
+};
+
+
+const actualizarRuta = (ruta,idconcurso,success, error) => {
+  var params = {
+    TableName: 'archivos',
+    FilterExpression : 'idconcurso = :idconcurso',
+    ExpressionAttributeValues : {':idconcurso': idconcurso} 
+  };
+        
+  ddb.scan(params,function(err,result){
+    if(err){
+      console.log(err);
+      error(err);
+    }else{
+      console.log('trajo datos --- '+JSON.stringify(result));
+      if(result.Items.length>0){
+        for(var i=0;i<result.Items.length;i++){
+               
+          var params = {
+            TableName: 'archivos',
+            Key:{
+              idarchivos: result.Items[i].idarchivos,
+              idconcurso: idconcurso
+            },
+            UpdateExpression : 'set ruta = :ruta',
+            ExpressionAttributeValues : {
+              ':ruta': ruta
+            },
+            'ReturnValues' : 'ALL_NEW'
+          };
+                
+          ddb.update(params,function(err,result){
+            if(err){
+              console.log(err);
+              error(err);
+            }else{
+              console.log('--  ACTUALIZAR ESTADO:'+JSON.stringify(result));
+            }
+                        
+          });
+        }  
+        success('ok');
+      }else{
+        success('ok');
+      }
+                       
+    }
+        
+  });
+};
+
+
+
+
+function envioCorreo(correo, urlConcurso) {
+  var request = email.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: {
+      personalizations: [
+        {
+          to: [
+            {
+              email: 'grupo8.cloud@gmail.com',
+            },
+          ],
+          subject: 'Voz procesada exitosamente',
+        },
+      ],
+      from: {
+        email: correo,
+      },
+      content: [
+        {
+          type: 'text/UTF-8',
+          value: `<html><body><h1>Voz Procesada!!</h1> <p>Tú voz ha sido procesada, en este <a href ="http://LoadBalancerWebServerD-480062229.us-east-1.elb.amazonaws.com/concurso/url/${urlConcurso}"> Concurso</a> ..Está lista para concursar!!'</p></body></html>` ,
+        },
+      ],
+    },
+  });
+
+  email.API(request, function(error, response) {
+    if (error) {
+      console.log('Error response received');
+    }
+    console.log(response.statusCode);
+    console.log(response.body);
+    console.log(response.headers);
+  });
+  
+
+}
+
+module.exports = {crearArchivo,actualizarEstado,obtenerArchxConcurso, obtenerArchxConcursoURL,actualizarRuta};
